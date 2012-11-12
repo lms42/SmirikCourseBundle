e9501a76a8f492c56d51182225bfd5a2d835adbe
Evgeny Smirnov <smirik@gmail.com>
2012-11-12 20:29:05 +0400

    Accept/reject via AJAX

--- Controller/AdminUserTaskController.php
+++ Controller/AdminUserTaskController.php
@@ -144,10 +144,58 @@ class AdminUserTaskController extends AbstractController
 	 */
 	public function acceptAction($id)
 	{
-		$user_task = UserTaskQuery::create()->findPk($id);
-		$user_task->setStatus(3);
+		$user_task = UserTaskQuery::create()
+		    ->joinLesson()
+		    ->joinTask()
+		    ->joinUser()
+		    ->findPk($id);
+
+		//$user_task->setStatus(3);
 		$user_task->save();
-		return $this->redirect($this->generateUrl('admin_users_tasks_index'));
+		//return $this->redirect($this->generateUrl('admin_users_tasks_index'));
+		return $this->render('SmirikCourseBundle:Admin/UserTask:accept.html.twig', array(
+		  'id' => $id,
+		  'user_task' => $user_task,
+		));
+	}
+	
+	/**
+	 * @Route("/admin/users_tasks/{id}/save_review", name="admin_users_tasks_save_review")
+	 */
+	public function saveReviewAction($id)
+	{
+	    if ($this->getRequest()->isXmlHttpRequest())
+		{
+		    $mark    = $this->getRequest()->request->get('mark', 5);
+		    $comment = $this->getRequest()->request->get('comment', false);
+		    $action  = $this->getRequest()->request->get('action', false);
+		    $user = $this->get('security.context')->getToken()->getUser();
+    		$user_task = UserTaskQuery::create()->findPk($id);
+		    if ($comment && $comment != '')
+		    {
+
+        		$user_task_review = new UserTaskReview();
+        		$user_task_review->setUserTaskId($id);
+        		$user_task_review->setUserId($user->getId());
+        		$user_task_review->setText($comment);
+        		$user_task_review->save();
+		    }
+		    
+		    if ($action == 'accept')
+		    {
+		        $user_task->setStatus(3);
+		        $user_task->setMark($mark);
+		    } elseif ($action == 'reject')
+		    {
+		        $user_task->setStatus(2);
+		    }
+		    $user_task->save();
+		    
+		    $data = array('result' => $id);
+		    return new Response(json_encode($data));
+		}
+	    $data = array('result' => false);
+		return new Response(json_encode($data));
 	}
 
 }
