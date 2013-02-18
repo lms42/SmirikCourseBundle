7e5d2cfe0b530a4b71d46a3c98284614a24c9234
Evgeny Smirnov <smirik@gmail.com>
2013-02-18 19:01:59 +0400

    Refactoring

--- Controller/AdminLessonController.php
+++ Controller/AdminLessonController.php
@@ -14,39 +14,6 @@ class AdminLessonController extends BaseController
 {
 
     /**
-     * @Template("SmirikCourseBundle:Admin/Lesson:edit.html.twig")
-     */
-    public function editAction($id)
-    {
-        $this->setup();
-        $this->generateRoutes();
-        $this->object = $this->getQuery()->findPk($id);
-        if (!$this->object) {
-            throw $this->createNotFoundException('Not found');
-        }
-
-        $request = $this->getRequest();
-
-        $form = $this->createForm($this->getForm(), $this->object);
-
-        if ('POST' == $request->getMethod()) {
-            $form->bindRequest($request);
-            if ($form->isValid()) {
-                $this->object->save();
-                return $this->redirect($this->generateUrl($this->routes['index']));
-            }
-        }
-
-        return array(
-            'layout'  => $this->layout,
-            'object'  => $this->object,
-            'form'    => $form->createView(),
-            'columns' => $this->grid->getColumns(),
-            'routes'  => $this->routes,
-        );
-    }
-
-    /**
      * @Route("/admin/lessons/{id}/assign", name="admin_lessons_assign")
      * @Template("SmirikCourseBundle:Admin/Lesson:assign.html.twig")
      */
